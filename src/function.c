42a11c1cdcd0cef64f01eb1c52c858802e0ee1e3
99003170 <75420372+99003170@users.noreply.github.com>
2020-12-05 11:46:31 +0530

    Update function.c

--- src/function.c
+++ src/function.c
@@ -1,20 +1,20 @@
 #include<function.h>
 float do_celsius_fahrenheit(float input1)
 {
-    float result = 9/5(input1)+32;
+    float result = 9/5*(input1)+32;
     return result;
 }
 
-float do_fahrenheit_kelvin(float input1)
+float do_kelvin_fahrenheit(float input1)
 {
-    float result = 9/5(input1-273)+32;
+    float result = 9/5*(input1-273)+32;
     return result;
 }
 
 
 float do_fahrenheit_celsius(float input1)
 {
-    float result = 5/9(input1-32);
+    float result = 5/9*(input1-32);
     return result;
 }
 
