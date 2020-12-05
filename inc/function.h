ed63dddf955e37e3854bc756b7b0e6e323c8f0e2
99003170 <75420372+99003170@users.noreply.github.com>
2020-12-05 11:48:47 +0530

    Update function.h

--- inc/function.h
+++ inc/function.h
@@ -4,7 +4,7 @@
 #define __FUNCTION_H__
 
 float do_celsius_fahrenheit(float);
-float do_fahrenheit_kelvin(float);
+float do_kelvin_fahrenheit(float);
 float do_fahrenheit_celsius(float);
 float do_celsius_kelvin(float);
 float do_kelvin_celsius(float);
